e8d3343e0f9075a432a952bf5c64492bc97ca091
Marc <mmaniscalco18@yahoo.com>
2013-05-22 13:15:43 -0400

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Marc Maniscalco"
+    email    "mmaniscalco18@yahoo.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
